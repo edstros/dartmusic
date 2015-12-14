d9a25793f638703896c092187d34e11e76dfcb81
Edwin Acevedo <edwin.a.acevedo@gmail.com>
2015-12-14 13:36:51 -0600

    WordPress commented, some code tweaks.

--- src/js/main.js
+++ src/js/main.js
@@ -1,34 +1,41 @@
 /*global $:false */
+
+/*first we cache the vars for speed and conveneience*/
 var $trigger = $('.trigger');
 var $target = $('.target');
+
+
 $(function () {
+/*it starts with a click on the button, which calls grow()*/
   $trigger.click(function () {
     grow();
   });
 
+/*grow () animates the circle*/
   function grow() {
     $target.animate({
       height: '200px',
       width: '200px'
-    }).delay(3000);
-    goBlue();
-    revert();
-    $trigger.hide();
+    }).delay(3000);//waits three seconds
+    goBlue();//turns blue
+    revert(); //then calls the function to go back to the smaller size and turn red
+    $trigger.hide();//i hid the button because i found that i could screw this code up when it was clicked multiple times
   }
 
+/*this turns the div blue*/
   function goBlue() {
     $target.css({
       backgroundColor: 'blue'
     });
   }
-
+/*this turns the div red*/
   function goRed() {
     $trigger.show();
     $target.css({
       backgroundColor: 'red'
     });
   }
-
+/*this is the callback to grow(), reverting the div back to original state*/
   function revert() {
     $target
       .animate({
